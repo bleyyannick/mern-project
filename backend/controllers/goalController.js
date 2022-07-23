f6447d8aafc32c1e6dc2b6a4400a180cabf37d0a
Yannick Bley <bleyyannickpro@gmail.com>
2022-07-23 07:11:05 +0200

    crud operations for REST API

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,18 +1,45 @@
-const getGoals = (req, res) => {
-  res.status(200).json({ message: "get goals" });
-};
+const asyncHandler = require("express-async-handler");
 
-const setGoal = (req, res) => {
-  res.status(200).json({ message: "set goal" });
-};
+const Goal = require("../models/goalModel");
 
-const updateGoal = (req, res) => {
-  res.status(200).json({ message: `Update ${req.params.id}` });
-};
+const getGoals = asyncHandler(async (req, res) => {
+  const goals = await Goal.find();
+  res.status(200).json(goals);
+});
 
-const deleteGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
+  res.status(200).json({ goal });
+});
+
+const updateGoal = asyncHandler(async (req, res) => {
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    throw new Error("Goal not found");
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updatedGoal);
+});
+
+const deleteGoal = asyncHandler(async (req, res) => {
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    throw new Error("Goal not found");
+  }
+
+  await goal.remove();
+
   res.status(200).json({ message: `Delete ${req.params.id}` });
-};
+});
 
 module.exports = {
   getGoals,
